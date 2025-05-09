e5475bbd39ead7290577a1ce70e15144d03efba7
Shraddha Raut <157194393+ShraddhaR03@users.noreply.github.com>
2025-05-09 11:43:28 +0530

    Create fifo_pkg.sv

---
+++ fifo_pkg.sv
@@ -0,0 +1,21 @@
+package fifo_pkg;
+
+`include "uvm_macros.svh" 
+import uvm_pkg::*;
+
+`include "seq_item.sv"
+`include "op_monitor.sv"
+`include "ip_monitor.sv"
+`include "ip_driver.sv"
+`include "ip_seqr.sv"
+`include "op_agent.sv"
+`include "ip_agent.sv"
+`include "scoreboard.sv"
+`include "coverage_collector.sv"
+`include "vseqr.sv"
+`include "env.sv"
+ `include "seq.sv"
+`include "vseq.sv"
+ `include "test.sv"
+
+endpackage
